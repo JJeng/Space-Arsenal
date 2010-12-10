5f52d524cfe5224732317240960e92769a2ffe85
Jeffrey Eng <JJeng2@gmail.com>
2010-12-10 00:53:35 -0500

    research notes

--- src/battleCruiser/alpha/View.java
+++ src/battleCruiser/alpha/View.java
@@ -1,5 +1,6 @@
 package battleCruiser.alpha;
 //view of an object in space regarding model, texture, size, and orientation
+//look into android.graphics.drawable and android.view.animation
 public class View {
 	float size;
 	Model model;
