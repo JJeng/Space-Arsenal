a924b6e96e39701efe2375b9e7e0504fdb8a0697
Jeffrey Eng <JJeng2@gmail.com>
2010-12-14 02:59:57 -0500

    working on menu

--- src/battleCruiser/alpha/MainMenu.java
+++ src/battleCruiser/alpha/MainMenu.java
@@ -1,24 +1,39 @@
 package battleCruiser.alpha;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class MainMenu extends JPanel{
+public class MainMenu extends JPanel {
 	JButton newGame;
 	JButton loadGame;
 	JButton multiplayer;
+	JButton quit;
 	public MainMenu(){
 		newGame = new JButton("New");
+		newGame.setName("New");
+		newGame.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				//TODO: Create New Character
+				//TODO: Load Relevant Game Data
+				//TODO: 
+			}
+		});
 		loadGame = new JButton("Load");
+		loadGame.setName("Load");
 		multiplayer = new JButton("Multiplayer");
+		multiplayer.setName("multi");
+		quit = new JButton("Quit");
+		quit.setName("quit");
 		this.setLayout(new BorderLayout());
 		this.add(newGame,BorderLayout.CENTER);
 		this.add(loadGame,BorderLayout.CENTER);
 		this.add(multiplayer,BorderLayout.CENTER);
+		this.add(quit,BorderLayout.CENTER);
 		this.add(new JTextField("Battle Cruiser"),BorderLayout.NORTH);
 	}
-
 }
